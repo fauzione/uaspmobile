99abb64d5d8b14a1a8240858fa9b82e00588aacf
fauzinurazis <fauziazis808@gmail.com>
2024-07-18 09:22:38 +0700

    neww

--- lib/register_mahasiswa.dart
+++ lib/register_mahasiswa.dart
@@ -12,65 +12,107 @@ class RegisterMahasiswaPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register Mahasiswa'),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _inputField('Username', _usernameController),
-            const SizedBox(height: 20),
-            _inputField('Password', _passwordController, isPassword: true),
-            const SizedBox(height: 20),
-            _inputField('Confirm Password', _confirmPasswordController,
-                isPassword: true),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () async {
-                String username = _usernameController.text.trim();
-                String password = _passwordController.text.trim();
-                String confirmPassword = _confirmPasswordController.text.trim();
-
-                if (username.isNotEmpty &&
-                    password.isNotEmpty &&
-                    confirmPassword.isNotEmpty) {
-                  if (password == confirmPassword) {
-                    // Simpan data ke Firestore dengan menambahkan dokumen
-                    await FirebaseFirestore.instance
-                        .collection('mahasiswa')
-                        .add({
-                      'username': username,
-                      'password': password,
-                    });
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Mahasiswa berhasil didaftarkan.'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.purpleAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Daftar Mahasiswa',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepPurple,
                       ),
-                    );
-
-                    // Kembali ke halaman login
-                    Navigator.pop(context);
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content:
-                            Text('Password dan Confirm Password harus sama.'),
-                      ),
-                    );
-                  }
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                          'Username, Password, dan Confirm Password harus diisi.'),
                     ),
-                  );
-                }
-              },
-              child: Text('Register'),
+                    const SizedBox(height: 20),
+                    _inputField('Username', _usernameController),
+                    const SizedBox(height: 20),
+                    _inputField('Password', _passwordController,
+                        isPassword: true),
+                    const SizedBox(height: 20),
+                    _inputField('Confirm Password', _confirmPasswordController,
+                        isPassword: true),
+                    const SizedBox(height: 50),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor:
+                            const Color.fromARGB(255, 255, 255, 255),
+                        backgroundColor: Colors.deepPurple,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                        textStyle: TextStyle(fontSize: 16),
+                      ),
+                      onPressed: () async {
+                        String username = _usernameController.text.trim();
+                        String password = _passwordController.text.trim();
+                        String confirmPassword =
+                            _confirmPasswordController.text.trim();
+
+                        if (username.isNotEmpty &&
+                            password.isNotEmpty &&
+                            confirmPassword.isNotEmpty) {
+                          if (password == confirmPassword) {
+                            // Simpan data ke Firestore dengan menambahkan dokumen
+                            await FirebaseFirestore.instance
+                                .collection('mahasiswa')
+                                .add({
+                              'username': username,
+                              'password': password,
+                            });
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                    Text('Mahasiswa berhasil didaftarkan.'),
+                              ),
+                            );
+
+                            // Kembali ke halaman login
+                            Navigator.pop(context);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Password dan Confirm Password harus sama.'),
+                              ),
+                            );
+                          }
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                  'Username, Password, dan Confirm Password harus diisi.'),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text('Register'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -81,7 +123,12 @@ class RegisterMahasiswaPage extends StatelessWidget {
     return TextField(
       controller: controller,
       decoration: InputDecoration(
-        hintText: hintText,
+        labelText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        filled: true,
+        fillColor: Colors.grey[200],
       ),
       obscureText: isPassword,
     );
